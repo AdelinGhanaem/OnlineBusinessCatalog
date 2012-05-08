870d6866931de365fa9f5a324d87bc333f1cf8bd
adio from laptop <webmaster1803@gmail.com>
2012-05-07 20:07:59 -0700

    asd

--- src/com/myonlinebd/catalog/client/RequestFactory/BusinessCardsRequestFactory.java
+++ src/com/myonlinebd/catalog/client/RequestFactory/BusinessCardsRequestFactory.java
@@ -4,11 +4,7 @@ import com.google.web.bindery.requestfactory.shared.Request;
 import com.google.web.bindery.requestfactory.shared.RequestContext;
 import com.google.web.bindery.requestfactory.shared.RequestFactory;
 import com.google.web.bindery.requestfactory.shared.Service;
-<<<<<<< HEAD
 import com.myonlinebd.catalog.server.AccountService;
-=======
-import com.myonlinebd.catalog.server.AccountServiceImpl;
->>>>>>> 2c4d87e22d71a808b8c8ebc8c4fb8ab1963497b7
 import com.myonlinebd.catalog.server.MyServicesLocator;
 import com.myonlinebd.catalog.shared.entities.AccountProxy;
 import com.myonlinebd.catalog.shared.entities.ResponseProxy;
@@ -29,6 +25,5 @@ public interface BusinessCardsRequestFactory extends RequestFactory {
   }
 
   AccountContext accountContext();
-
 
 }
